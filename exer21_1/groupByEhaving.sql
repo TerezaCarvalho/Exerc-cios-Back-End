413c15254792cd5b80e439a42a4f5dfe1dba952c
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-21 12:27:37 -0300

    Funções mais usadas no SQL

--- exer21_1/groupByEhaving.sql
+++ exer21_1/groupByEhaving.sql
@@ -80,3 +80,16 @@ SELECT first_name, COUNT(*) AS 'nomes cadastrados'
 FROM sakila.actor
 GROUP BY first_name
 HAVING 'nomes cadastrados' > 2;
+
+-- Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido)
+-- à coluna gerada por AVG(length), de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+    SELECT rating, AVG(length)
+    FROM sakila.film
+    GROUP BY rating;
+
+-- Usando a query a seguir, exiba apenas os valores de total de substituição de custo que estão acima
+-- de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost), de forma que deixe a query
+-- mais legível. Finalize ordenando os resultados de forma crescente.
+    SELECT rating, SUM(replacement_cost)
+    FROM sakila.film
+    GROUP by rating;
